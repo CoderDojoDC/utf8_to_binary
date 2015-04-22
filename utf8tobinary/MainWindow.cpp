a699213470df8b68d496e1013fe534e01cbf661e
Frank Hunleth <fhunleth@lkc.com>
2015-04-22 15:00:57 -0400

    Add UTF codepoints

--- utf8tobinary/MainWindow.cpp
+++ utf8tobinary/MainWindow.cpp
@@ -1,5 +1,6 @@
 #include "MainWindow.h"
 #include "ui_MainWindow.h"
+#include <QList>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -11,6 +12,44 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+static QList<int> utf8ToCodepoints(const QByteArray &utf8)
+{
+    QList<int> codepoints;
+
+    for (int i = 0; i < utf8.count(); i++) {
+        char b0 = utf8.at(i);
+        int value;
+        if ((b0 & 0xf8) == 0xf0) {
+            // 4 byte
+            value = ((b0 & 0x7) << 18) |
+                    ((utf8.at(i + 1) & 0x3f) << 12) |
+                    ((utf8.at(i + 2) & 0x3f) << 6) |
+                    (utf8.at(i + 3) & 0x3f);
+            i += 3;
+        } else if ((b0 & 0xf0) == 0xe0) {
+            // 3 byte
+            value = ((b0 & 0xf) << 12) |
+                    ((utf8.at(i + 1) & 0x3f) << 6) |
+                    (utf8.at(i + 2) & 0x3f);
+            i += 2;
+        } else if ((b0 & 0xe0) == 0xc0) {
+            // 2 byte
+            value = ((b0 & 0x1f) << 6) |
+                    (utf8.at(i + 1) & 0x3f);
+            i++;
+        } else if ((b0 & 0x80) == 0) {
+            // 1 byte
+            value = b0;
+        } else {
+            // Invalid
+            qCritical("Bad UTF-8 char");
+            value = 0;
+        }
+        codepoints << value;
+    }
+    return codepoints;
 }
 
 void MainWindow::on_inputLineEdit_textChanged(const QString &text)
@@ -32,5 +71,17 @@ void MainWindow::on_inputLineEdit_textChanged(const QString &text)
         }
         j++;
     }
-    ui->outputTextEdit->setPlainText(output);
+    ui->utf8BitsTextEdit->setPlainText(output);
+
+    j = 0;
+    output.clear();
+    QList<int> codepoints = utf8ToCodepoints(utf8);
+    foreach (int c, codepoints) {
+        if (j > 0)
+           output.append((j % 2) ? ", " : "\n");
+
+        output.append(QString("U+%1").arg(c, 4, 16, QLatin1Char('0')));
+        j++;
+    }
+    ui->unicodeCodePointsTextEdit->setPlainText(output);
 }
